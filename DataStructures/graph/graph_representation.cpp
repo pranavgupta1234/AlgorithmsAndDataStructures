558896391a819fc7137751ed3f3665f28b966fed
pranav_gupta <pranavgupta4321@gmail.com>
2017-03-18 16:06:47 +0530

    graph_rep completed

--- DataStructures/graph/graph_representation.cpp
+++ DataStructures/graph/graph_representation.cpp
@@ -4,18 +4,22 @@
 
 using namespace std;
 
+/* while using typedef in struct then c compiler needs the struct notation inside the struct body as it has not typedefed the value
+while it is not necessary in c++ compiler. But always remember to declare a struct before using in other(clear dependencies first). 
+For eg using ArrayList before GraphNode will cause error because ArrayList depends upon GraphNode.
+*/ 
 typedef struct GraphNode{
 	int data;
-	struct GraphNode* link;
+	GraphNode* link;
 }GraphNode;
 
 typedef struct ArrayList{
-	GraphNode* list;
+	GraphNode* head;
 }ArrayList;
 
 typedef struct Graph{
 	int V;
-	ArrayList* graph;
+	ArrayList* listNodes;
 
 }Graph;
 
@@ -29,6 +33,7 @@ int main(int argc,char* argv[]){
 	cout<<"Select the representation form of graph : "<<endl;
 	cout<<"1. Adjacency Matrix"<<endl;
 	cout<<"2. Adjacency List"<<endl;
+	cout<<"Your Choice :: ";
 	cin>>selection;
 	switch(selection){
 		case 1:
@@ -74,11 +79,80 @@ void display_matrix(int** graph){
 	}
 }
 
+//creating a graph with V vertices
 Graph* createGraph(int V){
-	Graph graph = 
+	
+	//dynamically allocate space for a graph
+	Graph* graph = (Graph*)malloc(sizeof(Graph));
+
+	//way to access fields of struct through pointer to it
+	graph->V = V;
+
+	graph->listNodes = (ArrayList*)malloc(V*sizeof(ArrayList));
+
+	//initialising all links to lists as null
+	for(int i=0;i<V;i++){
+		graph->listNodes[i].head = NULL;
+	}
+
+	return graph;
 }
+
+//function to create a node in graph as new node is added in front of linked list so only dest is required
+GraphNode* newGraphNode(int dest){
+
+	GraphNode* newnode = new GraphNode();
+	newnode->data = dest;
+	newnode -> link = NULL;
+
+	return newnode;
+}
+
+void addEdge(Graph* graph,int src,int dest){
+
+	GraphNode* newnode = newGraphNode(dest);
+
+	//add node fron src to destination
+	newnode->link = graph->listNodes[src].head;
+	graph->listNodes[src].head = newnode;
+
+	//as graph is undirected so create node from dest to src also
+	newnode = newGraphNode(src);
+	newnode->link = graph->listNodes[dest].head;
+	graph->listNodes[dest].head = newnode;
+}
+
+void printGraph(Graph* graph){
+
+	cout<<"This Graph has "<<graph->V<<" nodes"<<endl;
+
+	for(int i=0;i<graph->V;i++){
+
+		GraphNode* node = graph->listNodes[i].head;
+
+		cout<<i<<" HEAD -> ";
+
+		while(node!=NULL){
+
+			cout<<node->data<<" ";
+			node = node -> link;
+
+		}
+		cout<<endl;
+	}
+
+}
+
+
 void adjacency_list(){
 
+	Graph* graph = createGraph(5);
 
+	addEdge(graph,0,1);
+	addEdge(graph,2,3);
+	addEdge(graph,0,3);
+	addEdge(graph,1,4);
+
+	printGraph(graph);
 
 }
